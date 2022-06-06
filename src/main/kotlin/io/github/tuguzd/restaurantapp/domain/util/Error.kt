2831d417cd8c5a4bc99c793d97765a1b21d4e639
Damir Tugushev <0damir.1tugushev@gmail.com>
2022-06-06 14:08:08 +0300

    Add error handling classes

---
+++ src/main/kotlin/io/github/tuguzd/restaurantapp/domain/util/Error.kt
@@ -0,0 +1,6 @@
+package io.github.tuguzd.restaurantapp.domain.util
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+public data class Error(val message: String)
---
+++ src/main/kotlin/io/github/tuguzd/restaurantapp/domain/util/Result.kt
@@ -0,0 +1,54 @@
+package io.github.tuguzd.restaurantapp.domain.util
+
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+
+public sealed interface Result<S, E> {
+    public data class Success<S, E>(val data: S) : Result<S, E>
+
+    public data class Error<S, E>(val error: E?, val throwable: Throwable?) : Result<S, E>
+}
+
+public fun <S, E> Result<S, E>.dataOrNull(): S? = when (this) {
+    is Result.Error -> null
+    is Result.Success -> data
+}
+
+public fun <S, E> success(data: S): Result.Success<S, E> =
+    Result.Success(data)
+
+public fun <S, E> error(error: E?, throwable: Throwable?): Result.Error<S, E> =
+    Result.Error(error, throwable)
+
+@OptIn(ExperimentalContracts::class)
+public fun <S, E, N> Result<S, E>.mapSuccess(transform: (S) -> N): Result<N, E> {
+    contract { callsInPlace(transform, InvocationKind.AT_MOST_ONCE) }
+    return when (this) {
+        is Result.Error -> cast()
+        is Result.Success -> map(transform)
+    }
+}
+
+@OptIn(ExperimentalContracts::class)
+public fun <S, E, N> Result.Success<S, E>.map(transform: (S) -> N): Result.Success<N, E> {
+    contract { callsInPlace(transform, InvocationKind.EXACTLY_ONCE) }
+    return Result.Success(transform(data))
+}
+
+@Suppress("UNCHECKED_CAST")
+public fun <S, E, N> Result.Success<S, E>.cast(): Result.Success<S, N> =
+    this as Result.Success<S, N>
+
+@Suppress("UNCHECKED_CAST")
+public fun <S, E, N> Result.Error<S, E>.cast(): Result.Error<N, E> =
+    this as Result.Error<N, E>
+
+/**
+ * Converts [kotlin.Result] to data layer [Result].
+ */
+public fun <S, E> kotlin.Result<S>.toResult(): Result<S, E> =
+    when (val data = getOrNull()) {
+        null -> Result.Error(error = null, throwable = exceptionOrNull())
+        else -> Result.Success(data)
+    }
